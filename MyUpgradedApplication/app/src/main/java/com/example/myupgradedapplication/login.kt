dff6cf3a6d8724ae273099bdd60a203686dad2d8
Veselin <vesselin60@gmail.com>
2025-02-12 10:01:39 +0200

    6. Modificadores

--- MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/login.kt
+++ MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/login.kt
@@ -1,10 +1,21 @@
 package com.example.myupgradedapplication
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -14,19 +25,16 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(
-    widthDp = 50,
-    heightDp = 50,
-    showBackground = true,
-    showSystemUi = true,
-    device = Devices.PIXEL_4
-)
+@Preview()
 @Composable
 fun ExamplePreview() {
     Example(a = "Dochou")
 }
 @Composable
-fun Example(a:String = "Pepe") {
-    Text(text = "Gocho $a")
+fun Example(a:String) {
+    Text(text = "Gocho $a", modifier = Modifier
+        .background(Color.Red)
+        .padding(top = 30.dp)
+    )
 }
 
