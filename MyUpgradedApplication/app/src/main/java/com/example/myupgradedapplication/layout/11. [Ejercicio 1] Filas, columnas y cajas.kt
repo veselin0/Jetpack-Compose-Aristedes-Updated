4aed8b17934d1dbbc8177599a2cf1b7aa066b204
Veselin <vesselin60@gmail.com>
2025-02-14 16:17:50 +0200

    13. Spacer

--- MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/layout/11. [Ejercicio 1] Filas, columnas y cajas.kt
+++ MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/layout/11. [Ejercicio 1] Filas, columnas y cajas.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -25,6 +28,8 @@ fun My11Ejercicio1FilasColumnasYCajas(modifier: Modifier = Modifier) {
             Text(text = "Ejemplo1")
         }
 
+        Spacer(modifier = Modifier.height(20.dp))
+
             Row(modifier = Modifier.weight(1f)
                 .fillMaxWidth()) {
                 Box(modifier = Modifier
@@ -34,6 +39,9 @@ fun My11Ejercicio1FilasColumnasYCajas(modifier: Modifier = Modifier) {
                     contentAlignment = Alignment.Center){
                     Text(text = "Ejemplo2")
                 }
+
+                Spacer(modifier = Modifier.width(20.dp))
+
                 Box(modifier = Modifier
                     .weight(1f)
                     .fillMaxHeight()
@@ -43,6 +51,7 @@ fun My11Ejercicio1FilasColumnasYCajas(modifier: Modifier = Modifier) {
                 }
             }
 
+        Spacer(modifier = Modifier.height(20.dp))
 
         Box(modifier = Modifier.weight(1f)
             .fillMaxWidth()
