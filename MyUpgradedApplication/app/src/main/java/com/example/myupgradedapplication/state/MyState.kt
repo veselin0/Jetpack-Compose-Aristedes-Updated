e580737f0e759b7c77f48e311cb6bab54326de0e
Veselin <vesselin60@gmail.com>
2025-02-17 16:55:55 +0200

    19. Estados avanzados

--- MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/state/MyState.kt
+++ MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/state/MyState.kt
@@ -4,19 +4,18 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 
 @Composable
 fun MyState(modifier: Modifier = Modifier) {
-    val number = remember { mutableStateOf(0) }
+    var number by rememberSaveable { mutableIntStateOf(0) }
     Column {
-        Text(text = "Press me: ${number.value}", modifier = modifier.clickable { number.value += 1 })
-        Text(text = "Press me: ", modifier = modifier.clickable { number.value += 1 })
-        Text(text = "Press me:", modifier = modifier.clickable { number.value += 1 })
-        Text(text = "Press me: ${number.value}", modifier = modifier.clickable { number.value += 1 })
-        Text(text = "Press me:", modifier = modifier.clickable { number.value += 1 })
+        Text(text = "Press me: $number", modifier = modifier.clickable { number += 1 })
+        Text(text = "Press me: $number", modifier = modifier.clickable { number += 1 })
     }
 
 }
