37cbf94ba540044df7d7c331d14bf21b8eefe47c
Veselin <vesselin60@gmail.com>
2025-02-24 15:16:22 +0200

    26. Button

--- MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/MainActivity.kt
+++ MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.myupgradedapplication.components.MyButtons
 import com.example.myupgradedapplication.components.MyTextField
 import com.example.myupgradedapplication.components.MyTextFieldParent
 import com.example.myupgradedapplication.components.MyTexts
@@ -29,7 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyUpgradedApplicationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyTextFieldParent(modifier = Modifier.padding(innerPadding))
+                    MyButtons(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
---
+++ MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/components/Buttons.kt
@@ -0,0 +1,34 @@
+package com.example.myupgradedapplication.components
+
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MyButtons(modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Button(
+            onClick = { Log.i("Gocho", "Button pressed!") },
+            enabled = true,
+            shape = RoundedCornerShape(50),
+            border = BorderStroke(3.dp, Color.Magenta),
+            colors = ButtonDefaults.buttonColors(
+                contentColor = Color.Green,
+                containerColor = Color.Yellow,
+                disabledContentColor= Color.Red,
+                disabledContainerColor = Color.DarkGray
+            )
+        ) {
+            Text(text = "Press me!")
+        }
+    }
+}
