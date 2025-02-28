666a09ac3c0b479a22265e2e60c6d05fc358b2c8
Veselin <vesselin60@gmail.com>
2025-02-28 17:16:22 +0200

    61. Imágenes circulares

--- MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/components/Image.kt
+++ MyUpgradedApplication/app/src/main/java/com/example/myupgradedapplication/components/Image.kt
@@ -1,26 +1,56 @@
 package com.example.myupgradedapplication.components
 
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SegmentedButtonDefaults.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.myupgradedapplication.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MyImage(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(id = R.drawable.img_0017),
-        contentDescription = "Example",
-        modifier = Modifier.padding(50.dp),
-        alignment = Alignment.Center,
-        contentScale = ContentScale.Fit,
-        alpha = DefaultAlpha,
-        colorFilter = null
-    )
+fun MyImagesAndIcons(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.img_0017),
+            contentDescription = "Example",
+            modifier = Modifier.size(200.dp)
+        )
+
+        Spacer(modifier = modifier.height(5.dp))
+
+        Image(
+            painter = painterResource(id = R.drawable.img_0017),
+            contentDescription = "Example",
+            modifier = Modifier.size(200.dp).clip(CircleShape).border(5.dp, Color.Black, CircleShape)
+        )
+
+        
+    }
+
 }
+
